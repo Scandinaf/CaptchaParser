d760a8fdbcde98697e8f4026ef8d63f90be5bd5c
scandinaf_ <sbarouski@evolutiongaming.com>
2020-04-18 14:54:55 +0300

    renaming htmlParser -> imageParser

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 import calendar
 import time
 
-import ImageParser as htmlParser
+import ImageParser as imageParser
 
 for _ in range(0, 1):
     timestamp = calendar.timegm(time.gmtime())
-    htmlParser.collect_images_for_model('https://www.mvd.gov.by/api/captcha/main?unique=$timestamp', "./test/validation")
+    imageParser.collect_images_for_model('https://www.mvd.gov.by/api/captcha/main?unique=$timestamp',
+                                         "./test/validation")
     time.sleep(1)
